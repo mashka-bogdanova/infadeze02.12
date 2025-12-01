d1790247df60df9b909876e541f4f97763ec5118
maria <mavonadgob@gmail.com>
2025-12-02 01:00:26 +0300

    new curls, new methods

--- demo/src/main/java/com/example/demo/MessageController.java
+++ demo/src/main/java/com/example/demo/MessageController.java
@@ -11,14 +11,21 @@ import java.util.List;
 public class MessageController {
     private final List<String> messages = new ArrayList<>();
 
-    // curl --location 'localhost:8081/messages'
-    @GetMapping("messages")
-    public ResponseEntity<List<String>> getMessages() {
-        return ResponseEntity.ok(messages);
+    // curl --location 'localhost:8081/messages/mySubstring/yiou'
+    @GetMapping("messages/mySubstring/{substring}")
+    public ResponseEntity<List<String>> getMessages(@PathVariable String substring) {
+        List<String> messages_new = new ArrayList<>();
+        for(String s: messages){
+            if(s.contains(substring)){
+                messages_new.add(s);
+
+            }
+        }
+        return ResponseEntity.ok(messages_new);
     }
 
     //curl --location 'localhost:8081/messages' \
-    //--header 'Content-Type: text/plain' \
+    //--header 'Content-Type: application/json' \
     //--data '{
     //    йоу
     //}'
@@ -30,22 +37,20 @@ public class MessageController {
 
     //curl --location 'localhost:8081/messages/0'
     @GetMapping("messages/{index}")
-    public ResponseEntity<String> getMessage(@PathVariable("index") Integer
-                                                     index) {
+    public ResponseEntity<String> getMessage(@PathVariable("index") Integer index) {
         return ResponseEntity.ok(messages.get(index));
     }
 
     //curl --location --request DELETE 'localhost:8081/messages/0'
     @DeleteMapping("messages/{index}")
-    public ResponseEntity<Void> deleteText(@PathVariable("index") Integer
-                                                   index) {
+    public ResponseEntity<Void> deleteText(@PathVariable("index") Integer index) {
         messages.remove((int) index);
         return ResponseEntity.noContent().build();
     }
 
 
     //curl --location --request PUT 'localhost:8081/messages/0' \
-    //--header 'Content-Type: text/plain' \
+    //--header 'Content-Type: application/json' \
     //--data 'йоу1'
     @PutMapping("messages/{index}")
     public ResponseEntity<Void> updateMessage(
@@ -55,5 +60,55 @@ public class MessageController {
         messages.add(i, message);
         return ResponseEntity.accepted().build();
     }
+
+    //curl --location 'localhost:8081/messages/search/yiou'
+    @GetMapping("messages/search/{text}")
+    public ResponseEntity<Integer> getNewIndex(@PathVariable String text) {
+        List<String> messages_new = new ArrayList<>();
+        int ind = 0;
+        for(String s: messages){
+            if(s.equals(text)){
+                return ResponseEntity.ok(ind);
+            }
+            ind++;
+        }
+        return ResponseEntity.ok(-1);
+    }
+
+    //curl --location 'localhost:8081/messages/count'
+    @GetMapping("messages/count")
+    public ResponseEntity<Integer> counter(){
+        return ResponseEntity.ok(messages.size());
+    }
+
+    //curl --location 'localhost:8081/messages/0/create' \
+    //--header 'Content-Type: application/json' \
+    //--data 'yiou'
+    @PostMapping("messages/{index}/create")
+    public ResponseEntity<Void> createNewMessage(@PathVariable int index, @RequestBody String text){
+        messages.add(index, text);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    //curl --location --request DELETE 'localhost:8081/messages/search/yiou'
+    @DeleteMapping("messages/search/{text}")
+    public ResponseEntity<Void> deleteNewText(@PathVariable String text) {
+        int ind = 0;
+        for(String s: messages){
+            if(s.contains(text)){
+                messages.remove(ind);
+            }
+            ind++;
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
+
 }
 
